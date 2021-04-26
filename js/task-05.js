8f53a5253e7d7000b802011a68330f2d7adc20a9
sonya287 <74300825+sonya287@users.noreply.github.com>
2021-04-26 13:02:31 +0300

    26.04

--- js/task-05.js
+++ js/task-05.js
@@ -1,10 +1,12 @@
 const refs = {
-    input: document.querySelector("#name-input"),
-    output: document.querySelector("#name-output"),
+  input: document.querySelector('#name-input'),
+  output: document.querySelector('#name-output'),
 };
 
 const onInputChange = event => {
-    refs.output.textContent = event.currentTarget.value;
+  event.currentTarget.value !== ''
+    ? (refs.output.textContent = event.currentTarget.value)
+    : (refs.output.textContent = 'незнайомець');
 };
 
 refs.input.addEventListener('input', onInputChange);
